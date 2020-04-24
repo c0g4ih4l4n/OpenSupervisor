9bb1a6d9bbbeb5956c16616efe67933bb7762671
long <longtranf@gmail.com>
2020-04-24 22:48:48 +0700

    Start utils for nmap (mostly script scan)

--- Code/nmap_utils.py
+++ Code/nmap_utils.py
@@ -1,6 +1,22 @@
-import python-nmap
+import nmap
 
-def scan_all_port():
+# get all ip
+# pull script list with each service
+# write custom script for doing some shits
+# Http request smuggling
+
+# Detect web technology using whatweb or wappanalyzer
+# some common are nginx/phpfpm
+# apache solr
+# Scan cve with that technology
+
+
+def scan_all_port(ip_list):
+	port_range = '1-65535'
+	nm = nmap.PortScannerAsync()
+	for ip in ip_list:
+		nmap.PortScanner(ip, port_range, callback=test, sudo=False)
+		
 	pass
 
 def scan_service():
@@ -14,4 +30,11 @@ def script_scan():
 
 # use aquatone
 def visualization():
+	pass
+
+def parse_result(hosts, scan_data):
+	pass
+
+def check_host_alive():
+	
 	pass
